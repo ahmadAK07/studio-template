50a006e669515946547b84cd4102935b4e955d9c
ahmadAK07 <muhammadahmadamin512@gmail.com>
2024-06-30 10:31:52 +0500

    animation

--- src/gridpattren.js
+++ src/gridpattren.js
@@ -21,7 +21,7 @@ const blocksContainer = document.getElementById('blocksContainer');
 function createBlock(x, y, key = '') {
 const ns = 'http://www.w3.org/2000/svg';
 const path = document.createElementNS(ns, 'path');
-path.setAttribute('transform', `translate(${-32 * y + 96 * x} ${160 * y})`);
+path.setAttribute('transform', `translate(${-32 * y + 96 * x} ${160 * y + 96})`);
 path.setAttribute(
   'd',
   'M45.119 4.5a11.5 11.5 0 0 0-11.277 9.245l-25.6 128C6.82 148.861 12.262 155.5 19.52 155.5h63.366a11.5 11.5 0 0 0 11.277-9.245l25.6-128c1.423-7.116-4.02-13.755-11.277-13.755H45.119Z'
